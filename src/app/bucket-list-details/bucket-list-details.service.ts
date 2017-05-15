de51033fc885453d61b13204bbd96e24edd66140
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-15 19:24:55 +0300

    [Feature] Add bucket lists and bucket list details services

--- src/app/bucket-list-details/bucket-list-details.service.ts
+++ src/app/bucket-list-details/bucket-list-details.service.ts
@@ -1,27 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from "@angular/http";
-import {Observable} from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
 
 import {BucketList} from "./bucket-list-details";
+import {BucketListItem} from "./bucket-list-item";
 
 
 @Injectable()
 export class BucketListDetailsService {
-  // bucketListUrl: string;
-  // bucketLists : BucketLists;
-  // bucketlist : BucketList;
 
   constructor(private http: Http) { }
 
-  getBucketList(bucketListUrl: string): Observable<BucketList> {
+  // Get bucketlist
+  getBucketListService(bucketListUrl: string): Observable<BucketList> {
     return this.http
       .get(bucketListUrl, {headers: this.getHeaders()})
       .map((response: Response) => response.json());
   }
 
+  // Add a new bucket list item
+  addItemService (addBucketListItemUrl: string ,name: string): Observable<any> {
+    let payload = JSON.stringify({ "name": name }); // Stringify payload
+
+    return this.http.post(addBucketListItemUrl, payload, {headers: this.getHeaders()})
+      .map((response:Response) => response.json());
+  }
+
+  // Update a bucket list item
+  updateItemService (bucketListItem: BucketListItem, body: Object): Observable<any> {
+    let payload = JSON.stringify(body); // Stringify payload
+
+    return this.http.put(bucketListItem.url, payload, {headers: this.getHeaders()})
+      .map((response:Response) => response.json());
+  }
+
+  // Delete a bucket list item
+  deleteItemService (bucketListItem: BucketListItem): Observable<any> {
+    return this.http.delete(bucketListItem.url)
+      .map((response:Response) => response.json());
+  }
+
   private getHeaders() {
     let headers = new Headers();
     headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
     headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsImlhdCI6MTQ5NDU3NDQzNiwiZXhwIjoxNDk1MTc5MjM2fQ.eyJpZCI6Mn0.TISfxUTeHnKc5e5dMfS13XAXmyz4mHsK113TQPnE02c');
     return headers;
   }
