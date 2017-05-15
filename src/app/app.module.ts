3733cf451d62a0251b26d06963365d09b27034a3
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-15 19:31:46 +0300

    [Chore] Import Clarity UI module and styles and relativeDate Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { ClarityModule } from "clarity-angular";
 
 import { routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { BucketListsComponent } from './bucket-lists/bucket-lists.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { BucketListDetailsComponent } from './bucket-list-details/bucket-list-details.component';
+import {RelativeDatePipe} from "./app.utils";
 
 
 @NgModule({
@@ -15,13 +19,16 @@ import { BucketListDetailsComponent } from './bucket-list-details/bucket-list-de
     AppComponent,
     WelcomeComponent,
     BucketListsComponent,
-    BucketListDetailsComponent
+    BucketListDetailsComponent,
+    RelativeDatePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    BrowserAnimationsModule,
+    ClarityModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
