51e0ef06224eee7a80696c9a8b7e1a6a33963947
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-15 19:30:12 +0300

    [Chore] Remove test template bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { BucketListsService } from "./bucket-lists/bucket-lists.service";
-import { environment } from '../environments/environment';
 import { BucketListDetailsService } from "./bucket-list-details/bucket-list-details.service";
 
 
@@ -14,6 +13,4 @@ import { BucketListDetailsService } from "./bucket-list-details/bucket-list-deta
 
 
 export class AppComponent {
-  title = 'Hello World!';
-  apiUrl = environment.apiUrl;
 }
