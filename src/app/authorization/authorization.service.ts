f7948c9451df0f375275f9a90480ef963a489479
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-16 07:59:25 +0300

    [Feature] Add user login and registration service

---
+++ src/app/authorization/authorization.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+import { environment } from '../../environments/environment';
+import {UserRegister} from "./user-registration-details";
+
+
+@Injectable()
+export class AuthorizationService {
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(private http: Http) { }
+
+  registerUser(user: UserRegister): Observable<any>{
+    return this.http.post(this.apiUrl + '/auth/register', JSON.stringify(user), {headers: this.getHeaders()})
+      .map((response: Response) => response.json());
+  }
+
+  login(username: string, password: string) {
+    return this.http.post(this.apiUrl + '/auth/login', { username: username, password: password },
+      {headers: this.getHeaders()})
+      .map((response: Response) => {
+        // login successful if there's an authorization token in the response
+        let user = response.json();
+        if (user && user.token) {
+          // store user details and token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
+      });
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+  }
+
+  private getHeaders() {
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    return headers;
+  }
+}
